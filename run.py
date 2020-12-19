482f881c4c79696a439f477db33077e57abf3e8c
sangv07 <stha.sangv@gmail.com>
2020-12-19 14:54:41 -0800

    Fixed not importing db when running the app in Heroku

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import app
+from database import db
+
+db.init_app(app)
+
+# Create table with out file "create_Table" SQLAclchemy will create table if not exist
+@app.before_first_request
+def create_tables():
+    db.create_all()
